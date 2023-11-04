684f5f2c348bf631bf7ba7ff022093f20fd26f83
aleksandr semenov <aleksandr.semenovsky@gmail.com>
2023-11-04 19:51:59 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Александр Семенов on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
